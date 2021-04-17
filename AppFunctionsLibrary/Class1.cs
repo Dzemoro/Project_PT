087e611895108e510aeab26bdf175d2f5062fff8
Dzemoro <dellorpl@gmail.com>
2021-04-17 19:46:15 +0200

    Empty project

---
+++ AppFunctionsLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFunctionsLibrary
+{
+    public class Class1
+    {
+    }
+}
