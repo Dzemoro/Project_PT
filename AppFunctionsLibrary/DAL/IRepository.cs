ba9baf97404f9d1ff01ac4c9473545468930df0f
Dzemoro <dellorpl@gmail.com>
2021-04-17 20:41:40 +0200

    Database support

---
+++ AppFunctionsLibrary/DAL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFunctionsLibrary.DAL
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Delete(TEntity entityToDelete);
+        void Delete(object id);
+        TEntity GetByID(object id);
+        TEntity Insert(TEntity entity);
+        void Update(TEntity entityToUpdate);
+        void Commit();
+    }
+}
---
+++ AppFunctionsLibrary/Models/AppDatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer;
+
+namespace AppFunctionsLibrary.Models
+{
+    public class AppDatabaseContext : DbContext
+    {
+        public AppDatabaseContext() : base("name=AppDatabaseContext") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+    }
+}
