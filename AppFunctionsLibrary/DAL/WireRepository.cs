a799dcf152a005fd593157edeec2fc3fe2614a7a
Dzemoro <dellorpl@gmail.com>
2021-05-04 17:48:12 +0200

    Repositories added

---
+++ AppFunctionsLibrary/DAL/WireRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppFunctionsLibrary.Models;
+
+namespace AppFunctionsLibrary.DAL
+{
+    public class WireRepository : BaseRepository<Wire>
+    {
+        public WireRepository(AppDatabaseContext context) : base(context)
+        {
+
+        }
+    }
+}
