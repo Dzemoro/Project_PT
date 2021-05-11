a24d8f6e723de328967f4fad265183e2687c505a
konigin144 <kinga777s@gmail.com>
2021-05-11 23:35:12 +0200

    SQLite and EntityFramework

---
+++ AppGUI/DeviceManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppFunctionsLibrary.Models;
+using AppFunctionsLibrary.DAL;
+
+namespace AppGUI
+{
+    class DeviceManager
+    {
+        public DeviceRepository dr { get; set; }
+
+        public DeviceManager(AppDatabaseContext context)
+        {
+            this.dr = new DeviceRepository(context);
+        }
+
+        public bool AddDevice() { return false; }
+        public bool DeleteDevice() { return false; }
+        public bool UpdateDevice() { return false; }
+
+
+    }
+}
