82a0cf8729ffda90f4ead9397ff34fa10c99a731
Dzemoro <dellorpl@gmail.com>
2021-05-13 12:35:35 +0200

    Missing packages added

--- AppFunctionsLibrary/Models/AppDatabaseContext.cs
+++ AppFunctionsLibrary/Models/AppDatabaseContext.cs
@@ -1,22 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.SqlServer;
 
 namespace AppFunctionsLibrary.Models
 {
     public class AppDatabaseContext : DbContext
     {
-        public AppDatabaseContext() : base("name=AppDatabaseContext") { }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            throw new UnintentionalCodeFirstException();
-        }
+        public AppDatabaseContext(DbContextOptions<AppDatabaseContext> options) : base(options) { }
 
         public DbSet<Channel> Channels { get; set; }
         public DbSet<Connector> Connectors { get; set; }
