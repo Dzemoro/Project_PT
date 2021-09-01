008cf7c3263f6744ed0548f4a7fcaa29b10e5911
konigin144 <kinga777s@gmail.com>
2021-09-01 19:42:08 +0200

    test names

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -9,9 +9,9 @@ namespace UnitTests {
         [DataRow(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)]
         [DataRow(20, 0.491, 5, 0.35, 16, 60.09, 3, 0.491, 5, 0.35, 0, -26.7)]
         [DataRow(20, 0.491, 5, 0.35, 16, 88.05, 3, 0.491, 5, 0.35, 10.1, -64.76)]
-        public void CountingTest(double a, double b, double c, double d, double e, double f, double g, double h, double i, double j, double k, double l) {
-            double result = CountClass.CountTest(a, b, c, d, e, f, g, h, i, j, k);
-            Assert.AreEqual(result, l);
+        public void CountingTest(double powerT, double wireT, double lenghtT, double connectorT, double gainT, double fsl, double gainR, double wireR, double lengthR, double connectorR, double obstacles, double result) {
+            double testResult = CountClass.CountTest(powerT, wireT, lenghtT, connectorT, gainT, fsl, gainR, wireR, lengthR, connectorR, obstacles);
+            Assert.AreEqual(testResult, result);
         }
     }
 }
